87f8917e1ca26e75a49a5eb4cb72cd30845b489a
StormsWindy <jakeworld41@gmail.com>
2024-10-02 19:27:07 -0600

    argghhh

--- sensible/index.js
+++ sensible/index.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 function draw() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.beginPath();
     ctx.rect(20, 40, 50, 50);
     ctx.fillStyle = "#FF0000";
